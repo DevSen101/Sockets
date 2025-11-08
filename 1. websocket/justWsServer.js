2434ec24e45ca315a71b51d01d60cd85835b2ac6
Dev Sen <devksen.personal@gmail.com>
2025-11-08 16:14:28 +0530

    sending data between client and server

--- 1. websocket/justWsServer.js
+++ 1. websocket/justWsServer.js
@@ -1,12 +1,31 @@
+// Import HTTP and WebSocket modules
 const http = require('http')
-const websocket = require('ws')
+const ws = require('ws')
 
+// Create a basic HTTP server for handshake
 const server = http.createServer((req, res) => {
- res.end('I am connected')
+  res.end('I am connected') // Respond to normal HTTP requests
 })
 
-const wss = new websocket.WebSocketServer({server})
+// Create a WebSocket server bound to the same HTTP server
+const wss = new ws.WebSocketServer({ server })
 
+// Triggered when WebSocket headers are sent to the client
+// wss.on('headers', (headers, req) => {
+//   console.log(headers) // Log handshake headers
+// })
+
+// Triggered when a new WebSocket connection is established
+wss.on('connection', (ws, req) => {
+  ws.send('Welcome to the websocket server!') // Send greeting to client
+
+  // Listen for messages sent by client
+  ws.on('message', (data) => {
+    console.log(data.toString()) // Log message from client
+  })
+})
+
+// Start the HTTP + WebSocket server on port 8000
 server.listen(8000, () => {
- console.log('Server listening on port 8000!');
+  console.log('Server listening on port 8000!')
 })
