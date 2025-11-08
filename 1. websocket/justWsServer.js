3c876f6dcf4adea808db648273e1819743171ad1
Dev Sen <devksen.personal@gmail.com>
2025-11-08 15:36:27 +0530

    Initial commit: start a simple websocket server

---
+++ 1. websocket/justWsServer.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const websocket = require('ws')
+
+const server = http.createServer((req, res) => {
+ res.end('I am connected')
+})
+
+const wss = new websocket.WebSocketServer({server})
+
+server.listen(8000, () => {
+ console.log('Server listening on port 8000!');
+})
